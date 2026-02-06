5b4c637c490889f69d5a2e468fac3532ccafe359
Albert Song <albb0920@gmail.com>
2026-02-07 01:22:51 +0900

    Apply patches in FreeBSD ports
    From:
    https://gitlab.com/FreeBSD/freebsd-ports/-/tree/main/net/aquantia-atlantic-kmod/files

--- aq_main.c
+++ aq_main.c
@@ -196,8 +196,12 @@ static driver_t aq_driver = {
 	"aq", aq_methods, sizeof(struct aq_dev),
 };
 
+#if __FreeBSD_version >= 1400058
+DRIVER_MODULE(atlantic, pci, aq_driver, 0, 0);
+#else
 static devclass_t aq_devclass;
 DRIVER_MODULE(atlantic, pci, aq_driver, aq_devclass, 0, 0);
+#endif
 
 MODULE_DEPEND(atlantic, pci, 1, 1, 1);
 MODULE_DEPEND(atlantic, ether, 1, 1, 1);
@@ -379,7 +383,7 @@ static int aq_if_attach_pre(if_ctx_t ctx)
 #else
 	if_t ifp;
 	ifp = iflib_get_ifp(ctx);
-	ifp->if_capenable =  IFCAP_RXCSUM | IFCAP_TXCSUM | IFCAP_HWCSUM | IFCAP_TSO |
+	if_setcapenable(ifp,  IFCAP_RXCSUM | IFCAP_TXCSUM | IFCAP_HWCSUM | IFCAP_TSO |
 							  IFCAP_JUMBO_MTU | IFCAP_VLAN_HWFILTER |
 							  IFCAP_VLAN_MTU | IFCAP_VLAN_HWTAGGING |
 							  IFCAP_VLAN_HWCSUM;
@@ -718,7 +722,7 @@ static void aq_if_stop(if_ctx_t ctx)
 static uint64_t aq_if_get_counter(if_ctx_t ctx, ift_counter cnt)
 {
 	struct aq_dev *softc = iflib_get_softc(ctx);
-	struct ifnet *ifp = iflib_get_ifp(ctx);
+	if_t ifp = iflib_get_ifp(ctx);
 
 	switch (cnt) {
 	case IFCOUNTER_IERRORS:
@@ -776,7 +780,7 @@ static bool aq_is_mc_promisc_required(struct aq_dev *softc)
 static void aq_if_multi_set(if_ctx_t ctx)
 {
 	struct aq_dev *softc = iflib_get_softc(ctx);
-	struct ifnet  *ifp = iflib_get_ifp(ctx);
+	if_t ifp = iflib_get_ifp(ctx);
 	struct aq_hw  *hw = &softc->hw;
 	AQ_DBG_ENTER();
 #if __FreeBSD_version >= 1300054
@@ -786,9 +790,9 @@ static void aq_if_multi_set(if_ctx_t ctx)
 #endif
 	if (softc->mcnt >= AQ_HW_MAC_MAX)
 	{
-		aq_hw_set_promisc(hw, !!(ifp->if_flags & IFF_PROMISC),
+		aq_hw_set_promisc(hw, !!(if_getflags(ifp) & IFF_PROMISC),
 				  aq_is_vlan_promisc_required(softc),
-				  !!(ifp->if_flags & IFF_ALLMULTI) || aq_is_mc_promisc_required(softc));
+				  !!(if_getflags(ifp) & IFF_ALLMULTI) || aq_is_mc_promisc_required(softc));
 	}else{
 #if __FreeBSD_version >= 1300054
 		if_foreach_llmaddr(iflib_get_ifp(ctx), &aq_mc_filter_apply, softc);
@@ -810,7 +814,7 @@ static int aq_if_mtu_set(if_ctx_t ctx, uint32_t mtu)
 
 static void aq_if_media_status(if_ctx_t ctx, struct ifmediareq *ifmr)
 {
-	struct ifnet *ifp;
+	if_t ifp;
 
 	AQ_DBG_ENTER();
 
@@ -824,13 +828,13 @@ static void aq_if_media_status(if_ctx_t ctx, struct ifmediareq *ifmr)
 static int aq_if_media_change(if_ctx_t ctx)
 {
 	struct aq_dev *softc = iflib_get_softc(ctx);
-	struct ifnet *ifp = iflib_get_ifp(ctx);
+	if_t ifp = iflib_get_ifp(ctx);
 	int rc = 0;
 
 	AQ_DBG_ENTER();
 
 	/* Not allowd in UP state, since causes unsync of rings */
-	if ((ifp->if_flags & IFF_UP)){
+	if ((if_getflags(ifp) & IFF_UP)){
 		rc = EPERM;
 		goto exit;
 	}
